906c02ada36e706279b7572f2b6627d8e04d5666
scezen <123314390+scezen@users.noreply.github.com>
2023-03-17 10:08:20 +0100

    First Commit

---
+++ MultiVerse/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultiVerse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
